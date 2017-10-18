e895a01a1dc9c13f7111396a235056a082417be1
User <User@169.254.232.109>
2017-10-18 14:04:57 -0500

    I wanna be the very best Like no one ever was To catch them is my real test To train them is my cause I will travel across the land Searching far and wide Each Pokemon to understand The power that's inside Pokemon, (gotta catch them all) its you and me I know its my destiny Pokemon, oh, you're my best friend In a world we must defend Pokemon, (gotta catch them all) a heart so true Our courage will pull us through You teach me and I'll teach you (Po-ke-mon) Gotta catch 'em all Gotta catch 'em all Every challenge along the way With courage I will face I will battle every day To claim my rightful place Come with me, the time is right There's no better team Arm in arm we'll win the fight It's always been our dream Pokemon! (Gotta catch 'em all) It's you and me I know it's my destiny Pokemon! Oh, you're my best friend In a world we must defend Pokemon! (Gotta catch 'em all) A heart so true Our courage will pull us through You teach me and I'll teach you Pokemon! Gotta catch 'em all Gotta catch 'em all Gotta catch 'em all Gotta catch 'em all Gotta catch 'em all Pokemon! (Gotta catch 'em all) It's you and me I know it's my destiny Pokemon! Oh, you're my best friend In a world we must defend Pokemon! (Gotta catch 'em all) A heart so true Our courage will pull us through You teach me and I'll teach you POKEMON! Gotta catch'em all!! Gotta catch'em all!! POKEMON!!!

--- src/main/java/Colosseum.java
+++ src/main/java/Colosseum.java
@@ -44,7 +44,7 @@ public class Colosseum {
      * <p>
      * Requirements we should check the user for: <br>
      * - Hit points are between 1 and MAX_HIT_POINTS <br>
-     * - No more than 50 points are split between attack level and defense leve <br>
+     * - No more than 50 points are split between attack level and defense level <br>
      * - Attack level and defense level must have at least 1 point each <br>
      * Example of how this will look to the user:
      * <p>
@@ -74,6 +74,41 @@ public class Colosseum {
      */
     public static Pokemon buildPokemon() {
         Pokemon tempPokemon = new Pokemon();
+
+        myScan = new Scanner(System.in);
+
+        System.out.print("Please name your Pokemon: ");
+        tempPokemon.name = myScan.nextLine();
+        System.out.print("How many hit points will it have? (1-50): ");
+        int hit = myScan.nextInt();
+        while (hit <= 0 || hit > MAX_HIT_POINTS) {
+            System.out.print("Sorry. Hit points must be between 1 and 50: ");
+            hit = myScan.nextInt();
+        }
+        tempPokemon.hitPoints = hit;
+
+        System.out.println("Split fifty points between attack level and defense level");
+
+        System.out.print("Enter your attack lever (1-49): ");
+        hit = myScan.nextInt();
+        while (hit <= 0 || hit >= MAX_HIT_POINTS) {
+            System.out.print("Sorry. The attack level must be between 1 and 49: ");
+            hit = myScan.nextInt();
+        }
+        tempPokemon.attackLevel = hit;
+
+        System.out.print("Enter your defense lever (1-"
+                        + (MAX_HIT_POINTS - tempPokemon.attackLevel) + "): ");
+        hit = myScan.nextInt();
+        while (hit <= 0 || hit > (MAX_HIT_POINTS - tempPokemon.attackLevel)) {
+            System.out.print("Sorry. The defense level must be between 1 and "
+                            + (MAX_HIT_POINTS - tempPokemon.attackLevel) + ": ");
+            hit = myScan.nextInt();
+        }
+        tempPokemon.defenseLevel = hit;
+
+        //myScan.close();
+
         return tempPokemon;
     }
 
@@ -91,7 +126,13 @@ public class Colosseum {
      * Implement this function.
      */
     public static void printWhoIsAhead() {
-        System.out.println("Implement me!");
+        if (firstPokemon.hitPoints > secondPokemon.hitPoints) {
+            System.out.println(firstPokemon.name + " is currently ahead!");
+        } else if (firstPokemon.hitPoints < secondPokemon.hitPoints) {
+            System.out.println(secondPokemon.name + " is currently ahead!");
+        } else {
+            System.out.println("Holy heck, Batman! There's a tie!");
+        }
     }
 
     /**
@@ -102,7 +143,13 @@ public class Colosseum {
      * Write this function.
      */
     public static void determineWinner() {
-        System.out.println("Implement me!");
+        if (firstPokemon.hitPoints > secondPokemon.hitPoints) {
+            System.out.println(firstPokemon.name + " has won!");
+        } else if (firstPokemon.hitPoints < secondPokemon.hitPoints) {
+            System.out.println(secondPokemon.name + " has won!");
+        } else {
+            System.out.println("Holy heck, Batman! This shouldn't be printing!");
+        }
     }
 
     /**
@@ -114,14 +161,14 @@ public class Colosseum {
         System.out.println("Player 1, build your Pokemon!");
         System.out.println("=================");
         firstPokemon = buildPokemon();
-        firstPokemon.name = "Chuchu";
+        //firstPokemon.name = "Chuchu";
 
         System.out.println("");
 
         System.out.println("Player 2, build your Pokemon!");
         System.out.println("==================");
         secondPokemon = buildPokemon();
-        secondPokemon.name = "Xyz";
+        //secondPokemon.name = "Xyz";
     }
 
     /**
